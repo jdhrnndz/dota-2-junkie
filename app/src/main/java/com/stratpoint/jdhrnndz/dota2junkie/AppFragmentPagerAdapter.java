1b95d977d7c0d7b88212c970580a35239da362ed
jdhrnndz <johndeniellehernandez@gmail.com>
2016-07-20 14:24:23 +0800

    Created classes that holds logic for the tab layout

---
+++ app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/AppFragmentPagerAdapter.java
@@ -0,0 +1,35 @@
+package com.stratpoint.jdhrnndz.dota2junkie;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by johndeniellehernandez on 7/20/16.
+ */
+public class AppFragmentPagerAdapter extends FragmentPagerAdapter {
+    final int PAGE_COUNT = 3;
+    private String tabTitles[] = new String[] {"Profile", "Matches", "Play Style"};
+    private Context context;
+
+    public AppFragmentPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return PageFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabTitles[position];
+    }
+}
