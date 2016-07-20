4b0c3f1fc8689fb2636a014157d3e987ece59d7c
jdhrnndz <johndeniellehernandez@gmail.com>
2016-07-20 14:30:56 +0800

    Initialization and linking of ViewPager with its adapter and TabLayout to ViewPager

--- app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/MainActivity.java
+++ app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/MainActivity.java
@@ -1,5 +1,7 @@
 package com.stratpoint.jdhrnndz.dota2junkie;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new AppFragmentPagerAdapter(getSupportFragmentManager(), MainActivity.this));
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
