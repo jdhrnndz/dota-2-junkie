7d7fb2d50bffd19ba815b44bdc60700a58c726ba
jdhrnndz <johndeniellehernandez@gmail.com>
2016-07-21 14:27:12 +0800

    Removed unnecessary imports

--- app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/AppFragmentPagerAdapter.java
+++ app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/AppFragmentPagerAdapter.java
@@ -4,9 +4,6 @@ import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.TextView;
 
 /**
  * Created by johndeniellehernandez on 7/20/16.
