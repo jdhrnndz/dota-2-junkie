3ea00de436456407e3c6239a59c8ae7f7c1196f7
jdhrnndz <johndeniellehernandez@gmail.com>
2016-07-21 14:32:26 +0800

    Implemented adapter for Matches Recycler View

---
+++ app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/MatchesAdapter.java
@@ -0,0 +1,45 @@
+package com.stratpoint.jdhrnndz.dota2junkie;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by johndeniellehernandez on 7/20/16.
+ */
+public class MatchesAdapter extends RecyclerView.Adapter<MatchesAdapter.ViewHolder> {
+    private String[] mDataset;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        public View mView;
+        public ViewHolder(View v) {
+            super(v);
+            mView = v;
+        }
+    }
+
+    public MatchesAdapter(String[] matches) {
+        mDataset = matches;
+    }
+
+    @Override
+    public MatchesAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_match_item, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        ((TextView) holder.mView.findViewById(R.id.match_id)).setText(mDataset[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
