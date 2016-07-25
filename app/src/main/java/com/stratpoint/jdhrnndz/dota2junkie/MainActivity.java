f83a49739705a352232dee574a7d1714f8c04a58
jdhrnndz <johndeniellehernandez@gmail.com>
2016-07-25 14:42:42 +0800

    Refactored MainActivity
    - Fixed MainActivity attributes
    - Implemented onBackPressed, hardware back button now goes back to the previous fragment or to the previous activity from the first fragment

--- app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/MainActivity.java
+++ app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/MainActivity.java
@@ -6,16 +6,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
-        viewPager.setAdapter(new AppFragmentPagerAdapter(getSupportFragmentManager(), MainActivity.this));
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mViewPager.setAdapter(new AppFragmentPagerAdapter(getSupportFragmentManager(), MainActivity.this));
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
-        tabLayout.setupWithViewPager(viewPager);
+        mTabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+    @Override
+    public void onBackPressed() {
+        int currentItem = mViewPager.getCurrentItem();
+        if (currentItem == 0) {
+            super.onBackPressed();
+        } else {
+          mViewPager.setCurrentItem(currentItem - 1);
+        }
     }
 }
