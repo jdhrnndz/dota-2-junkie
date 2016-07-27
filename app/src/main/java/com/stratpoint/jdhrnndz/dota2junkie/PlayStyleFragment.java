2f7ff4fd88e176cfe7c24d04ce2668c5b9017a60
jdhrnndz <johndeniellehernandez@gmail.com>
2016-07-27 14:58:17 +0800

    Added sample chart in Play Style Fragment

--- app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/PlayStyleFragment.java
+++ app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/PlayStyleFragment.java
@@ -1,5 +1,6 @@
 package com.stratpoint.jdhrnndz.dota2junkie;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,6 +25,8 @@ import java.util.ArrayList;
 public class PlayStyleFragment extends BaseFragment {
     private final static int LAYOUT = R.layout.fragment_play_style;
 
+    private RadarChart mPlayStyleChart;
+
     public static PlayStyleFragment newInstance() {
         Bundle args = BaseFragment.initBundle(LAYOUT);
         PlayStyleFragment fragment = new PlayStyleFragment();
@@ -34,7 +37,42 @@ public class PlayStyleFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(mLayout, container, false);
+        mPlayStyleChart = (RadarChart) view.findViewById(R.id.play_style_chart);
+        mPlayStyleChart.setRotationEnabled(false);
+        mPlayStyleChart.setDescription("Recent 20 Matches");
+
+        setData();
 
         return view;
+    }
+
+    public void setData() {
+        ArrayList<RadarEntry> entries1 = new ArrayList<RadarEntry>();
+
+        entries1.add(new RadarEntry(0.15f, "FIGHTING"));
+        entries1.add(new RadarEntry(0.65f, "FARMING"));
+        entries1.add(new RadarEntry(0.60f, "SUPPORTING"));
+        entries1.add(new RadarEntry(0.35f, "PUSHING"));
+        entries1.add(new RadarEntry(0.70f, "VERSATILITY"));
+
+        RadarDataSet set1 = new RadarDataSet(entries1, "Last Week");
+        set1.setColor(Color.rgb(103, 110, 129));
+        set1.setFillColor(Color.rgb(103, 110, 129));
+        set1.setDrawFilled(true);
+        set1.setFillAlpha(180);
+        set1.setLineWidth(2f);
+        set1.setDrawHighlightCircleEnabled(true);
+        set1.setDrawHighlightIndicators(false);
+
+        ArrayList<IRadarDataSet> sets = new ArrayList<IRadarDataSet>();
+        sets.add(set1);
+
+        RadarData data = new RadarData(sets);
+        data.setValueTextSize(8f);
+        data.setDrawValues(false);
+        data.setValueTextColor(Color.WHITE);
+
+        mPlayStyleChart.setData(data);
+        mPlayStyleChart.invalidate();
     }
 }
