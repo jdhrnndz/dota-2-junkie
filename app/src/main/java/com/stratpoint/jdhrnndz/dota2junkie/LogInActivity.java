94c0ee565d29950e2c3bf0e952028d2d5a9cee1c
jdhrnndz <johndeniellehernandez@gmail.com>
2016-07-27 15:06:07 +0800

    Created a Log In Activity

---
+++ app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/LogInActivity.java
@@ -0,0 +1,16 @@
+package com.stratpoint.jdhrnndz.dota2junkie;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by johndeniellehernandez on 7/27/16.
+ */
+public class LogInActivity extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
