91af389b16fad0b3453a56e92ba76e234b922e71
jdhrnndz <johndeniellehernandez@gmail.com>
2016-07-27 15:28:56 +0800

    Added scroll behavior in the Main Activity

--- app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/MainActivity.java
+++ app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/MainActivity.java
@@ -1,9 +1,17 @@
 package com.stratpoint.jdhrnndz.dota2junkie;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private ViewPager mViewPager;
@@ -14,10 +22,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        final ActionBar ab = getSupportActionBar();
+//        ab.setHomeAsUpIndicator(R.drawable.icon_last_log_off);
+//        ab.setDisplayHomeAsUpEnabled(true);
+
         mViewPager = (ViewPager) findViewById(R.id.viewpager);
         mViewPager.setAdapter(new AppFragmentPagerAdapter(getSupportFragmentManager(), MainActivity.this));
 
-        mTabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        mTabLayout = (TabLayout) findViewById(R.id.tabs);
         mTabLayout.setupWithViewPager(mViewPager);
     }
 
