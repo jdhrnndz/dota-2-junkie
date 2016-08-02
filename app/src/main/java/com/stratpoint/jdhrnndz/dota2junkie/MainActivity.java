928895ed0a784aecc92e2064ab16c8a6e4056011
jdhrnndz <johndeniellehernandez@gmail.com>
2016-08-02 17:50:54 +0800

    Organized imports, added some TODO

--- app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/MainActivity.java
+++ app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/MainActivity.java
@@ -1,17 +1,10 @@
 package com.stratpoint.jdhrnndz.dota2junkie;
 
-import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private ViewPager mViewPager;
@@ -20,11 +13,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // TODO: Set "Steam ID" label as the center of the user info grid layout
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // TODO: Fix action bar scroll behavior to be ignored by user profile and play style tabs
 //        final ActionBar ab = getSupportActionBar();
 //        ab.setHomeAsUpIndicator(R.drawable.icon_last_log_off);
 //        ab.setDisplayHomeAsUpEnabled(true);
