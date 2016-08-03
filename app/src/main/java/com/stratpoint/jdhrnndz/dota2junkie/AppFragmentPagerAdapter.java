83ee008ff2c9979843d35d3c2088fb64a90f9eb9
jdhrnndz <johndeniellehernandez@gmail.com>
2016-08-03 11:33:51 +0800

    Used TabFragment enum

--- app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/AppFragmentPagerAdapter.java
+++ app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/AppFragmentPagerAdapter.java
@@ -10,12 +10,9 @@ import android.support.v4.app.FragmentPagerAdapter;
  */
 public class AppFragmentPagerAdapter extends FragmentPagerAdapter {
     final int PAGE_COUNT = 3;
-    private String tabTitles[] = new String[] {"Profile", "Matches", "Play Style"};
-    private Context context;
 
     public AppFragmentPagerAdapter(FragmentManager fm, Context context) {
         super(fm);
-        this.context = context;
     }
 
     @Override
@@ -28,14 +25,14 @@ public class AppFragmentPagerAdapter extends FragmentPagerAdapter {
         Fragment mFragment = null;
 
         switch(position) {
-            case 0: // Profile Fragment
-                mFragment = ProfileFragment.newInstance();
+            case 0:
+                mFragment = TabFragment.PROFILE.getFragment();
                 break;
-            case 1: // Matches Fragment
-                mFragment = MatchesFragment.newInstance();
+            case 1:
+                mFragment = TabFragment.MATCHES.getFragment();
                 break;
-            case 2: // Play Style Fragment
-                mFragment = PlayStyleFragment.newInstance();
+            case 2:
+                mFragment = TabFragment.PLAY_STYLE.getFragment();
                 break;
         }
         return mFragment;
@@ -43,6 +40,20 @@ public class AppFragmentPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public CharSequence getPageTitle(int position) {
-        return tabTitles[position];
+        String title = "";
+
+        switch(position) {
+            case 0:
+                title = TabFragment.PROFILE.getTitle();
+                break;
+            case 1:
+                title = TabFragment.MATCHES.getTitle();
+                break;
+            case 2:
+                title = TabFragment.PLAY_STYLE.getTitle();
+                break;
+        }
+
+        return title;
     }
 }
