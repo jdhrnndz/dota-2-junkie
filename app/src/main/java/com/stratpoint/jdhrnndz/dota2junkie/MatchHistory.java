f9ff44a8eb3627482204625e55d490a864291423
jdhrnndz <johndeniellehernandez@gmail.com>
2016-08-04 17:56:29 +0800

    Converted classes used in these files as inner classes.

--- app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/MatchHistory.java
+++ app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/MatchHistory.java
@@ -9,135 +9,89 @@ public class MatchHistory {
     public Result getResult() {
         return result;
     }
-}
 
-class Result {
-    private int status;
-    private int num_results;
-    private int total_results;
-    private int results_remaining;
-    private Match[] matches;
+    public static class Result {
+        private int status;
+        private int num_results;
+        private int total_results;
+        private int results_remaining;
+        private Match[] matches;
 
-    public int getStatus() {
-        return status;
-    }
-
-    public int getNum_results() {
-        return num_results;
-    }
-
-    public int getTotal_results() {
-        return total_results;
-    }
-
-    public int getResults_remaining() {
-        return results_remaining;
-    }
-
-    public Match[] getMatches() {
-        return matches;
-    }
-
-    @Override
-    public String toString() {
-//        String matchesString = "[";
-//        for (Match m : matches) {
-//            matchesString += m.toString();
-//        }
-//        matchesString += "]";
-
-        return "MatchHistory{" +
-                "status=" + status +
-                ", num_results=" + num_results +
-                ", total_results=" + total_results +
-                ", results_remaining=" + results_remaining +
-//                ", matches=" + matchesString +
-                '}';
-    }
-}
-
-class Match {
-    private long match_id;
-    private long match_seq_num;
-    private long start_time;
-    private int lobby_type;
-    private long radiant_team_id;
-    private long dire_team_id;
-    private MatchPlayer[] players;
-
-    public long getMatch_id() {
-        return match_id;
-    }
-
-    public long getMatch_seq_num() {
-        return match_seq_num;
-    }
-
-    public long getStart_time() {
-        return start_time;
-    }
-
-    public int getLobby_type() {
-        return lobby_type;
-    }
-
-    public long getRadiant_team_id() {
-        return radiant_team_id;
-    }
-
-    public long getDire_team_id() {
-        return dire_team_id;
-    }
-
-    public MatchPlayer[] getPlayers() {
-        return players;
-    }
-
-    @Override
-    public String toString() {
-        String playersString = "[";
-
-        for(MatchPlayer mp : players) {
-            playersString += players.toString();
+        public int getStatus() {
+            return status;
         }
 
-        playersString += "]";
+        public int getNum_results() {
+            return num_results;
+        }
 
-        return "Match{" +
-                "match_id=" + match_id +
-                ", match_seq_num=" + match_seq_num +
-                ", start_time=" + start_time +
-                ", lobby_type=" + lobby_type +
-                ", radiant_team_id=" + radiant_team_id +
-                ", dire_team_id=" + dire_team_id +
-                ", players=" + playersString +
-                '}';
+        public int getTotal_results() {
+            return total_results;
+        }
+
+        public int getResults_remaining() {
+            return results_remaining;
+        }
+
+        public Match[] getMatches() {
+            return matches;
+        }
+    }
+
+    public static class Match {
+        private long match_id;
+        private long match_seq_num;
+        private long start_time;
+        private int lobby_type;
+        private long radiant_team_id;
+        private long dire_team_id;
+        private MatchPlayer[] players;
+
+        public long getMatch_id() {
+            return match_id;
+        }
+
+        public long getMatch_seq_num() {
+            return match_seq_num;
+        }
+
+        public long getStart_time() {
+            return start_time;
+        }
+
+        public int getLobby_type() {
+            return lobby_type;
+        }
+
+        public long getRadiant_team_id() {
+            return radiant_team_id;
+        }
+
+        public long getDire_team_id() {
+            return dire_team_id;
+        }
+
+        public MatchPlayer[] getPlayers() {
+            return players;
+        }
+    }
+
+    public static class MatchPlayer {
+        private long account_id;
+        private short player_slot;
+        private int hero_id;
+
+        public long getAccount_id() {
+            return account_id;
+        }
+
+        public short getPlayer_slot() {
+            return player_slot;
+        }
+
+        public int getHero_id() {
+            return hero_id;
+        }
     }
 }
 
-class MatchPlayer {
-    private long account_id;
-    private short player_slot;
-    private int hero_id;
-
-    public long getAccount_id() {
-        return account_id;
-    }
-
-    public short getPlayer_slot() {
-        return player_slot;
-    }
-
-    public int getHero_id() {
-        return hero_id;
-    }
-
-    @Override
-    public String toString() {
-        return "MatchPlayer{" +
-                "account_id=" + account_id +
-                ", player_slot=" + player_slot +
-                ", hero_id=" + hero_id +
-                '}';
-    }
-}
--- app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/PlayerSummary.java
+++ app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/PlayerSummary.java
@@ -9,100 +9,101 @@ public class PlayerSummary {
     public DotaResponse getResponse() {
         return response;
     }
-}
 
-class DotaResponse {
-    private DotaPlayer[] players;
+    public static class DotaResponse {
+        private DotaPlayer[] players;
 
-    public DotaPlayer[] getPlayers() {
-        return players;
+        public DotaPlayer[] getPlayers() {
+            return players;
+        }
+    }
+
+    public static class DotaPlayer {
+        private String steamid;
+        private short communityvisibilitystate;
+        private short profilestate;
+        private String personaname;
+        private long lastlogoff;
+        private String profileurl;
+        private String avatar;
+        private String avatarmedium;
+        private String avatarfull;
+        private short personastate;
+        private String realname;
+        private String primaryclanid;
+        private long timecreated;
+        private short personastateflags;
+        private String loccountrycode;
+        private String locstatecode;
+        private long loccityid;
+
+        public String getSteamid() {
+            return steamid;
+        }
+
+        public short getCommunityvisibilitystate() {
+            return communityvisibilitystate;
+        }
+
+        public short getProfilestate() {
+            return profilestate;
+        }
+
+        public String getPersonaname() {
+            return personaname;
+        }
+
+        public long getLastlogoff() {
+            return lastlogoff;
+        }
+
+        public String getProfileurl() {
+            return profileurl;
+        }
+
+        public String getAvatar() {
+            return avatar;
+        }
+
+        public String getAvatarmedium() {
+            return avatarmedium;
+        }
+
+        public String getAvatarfull() {
+            return avatarfull;
+        }
+
+        public short getPersonastate() {
+            return personastate;
+        }
+
+        public String getRealname() {
+            return realname;
+        }
+
+        public String getPrimaryclanid() {
+            return primaryclanid;
+        }
+
+        public long getTimecreated() {
+            return timecreated;
+        }
+
+        public short getPersonastateflags() {
+            return personastateflags;
+        }
+
+        public String getLoccountrycode() {
+            return loccountrycode;
+        }
+
+        public String getLocstatecode() {
+            return locstatecode;
+        }
+
+        public long getLoccityid() {
+            return loccityid;
+        }
     }
 }
 
-class DotaPlayer {
-    private String steamid;
-    private short communityvisibilitystate;
-    private short profilestate;
-    private String personaname;
-    private long lastlogoff;
-    private String profileurl;
-    private String avatar;
-    private String avatarmedium;
-    private String avatarfull;
-    private short personastate;
-    private String realname;
-    private String primaryclanid;
-    private long timecreated;
-    private short personastateflags;
-    private String loccountrycode;
-    private String locstatecode;
-    private long loccityid;
-
-    public String getSteamid() {
-        return steamid;
-    }
-
-    public short getCommunityvisibilitystate() {
-        return communityvisibilitystate;
-    }
-
-    public short getProfilestate() {
-        return profilestate;
-    }
-
-    public String getPersonaname() {
-        return personaname;
-    }
-
-    public long getLastlogoff() {
-        return lastlogoff;
-    }
-
-    public String getProfileurl() {
-        return profileurl;
-    }
-
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public String getAvatarmedium() {
-        return avatarmedium;
-    }
-
-    public String getAvatarfull() {
-        return avatarfull;
-    }
-
-    public short getPersonastate() {
-        return personastate;
-    }
-
-    public String getRealname() {
-        return realname;
-    }
-
-    public String getPrimaryclanid() {
-        return primaryclanid;
-    }
-
-    public long getTimecreated() {
-        return timecreated;
-    }
-
-    public short getPersonastateflags() {
-        return personastateflags;
-    }
-
-    public String getLoccountrycode() {
-        return loccountrycode;
-    }
-
-    public String getLocstatecode() {
-        return locstatecode;
-    }
-
-    public long getLoccityid() {
-        return loccityid;
-    }
-}
