3f5f9741478fba2cb84d85142672f48363e5724e
jdhrnndz <johndeniellehernandez@gmail.com>
2016-08-05 13:15:12 +0800

    Converted onReceiveResponse method to more specific methods

--- app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/network/DotaApiResponseListener.java
+++ app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/network/DotaApiResponseListener.java
@@ -1,12 +1,15 @@
 package com.stratpoint.jdhrnndz.dota2junkie.network;
 
 import com.android.volley.VolleyError;
+import com.stratpoint.jdhrnndz.dota2junkie.MatchHistory;
 
 /**
  * Created by johndeniellehernandez on 8/4/16.
  */
 public interface DotaApiResponseListener {
-    void onReceiveResponse(int statusCode, String responseString, String action, String cookie);
+    void onReceiveStringResponse(int statusCode, String response);
+
+    void onReceiveMatchHistoryResponse(int statusCode, MatchHistory response);
 
     void onReceiveErrorResponse(int statusCode, VolleyError error);
 }
