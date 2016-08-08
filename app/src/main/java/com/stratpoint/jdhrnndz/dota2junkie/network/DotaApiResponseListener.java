9e437204263acec6f13f968ed4bdf3dc0d6b5e98
JD Hernandez <jhernandez@stratpoint.com>
2016-08-08 15:05:58 +0800

    Added comments

--- app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/network/DotaApiResponseListener.java
+++ app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/network/DotaApiResponseListener.java
@@ -3,7 +3,10 @@ package com.stratpoint.jdhrnndz.dota2junkie.network;
 import com.android.volley.VolleyError;
 
 /**
- * Created by johndeniellehernandez on 8/4/16.
+ * Author: John Denielle F. Hernandez
+ * Date: 8/4/16
+ * Description: Interface for classes that does the heavy-lifting in processing the responses. Don't
+ * let the ApiManager do the processing. 
  */
 public interface DotaApiResponseListener {
     void onReceiveResponse(int statusCode, Object response, int type);
