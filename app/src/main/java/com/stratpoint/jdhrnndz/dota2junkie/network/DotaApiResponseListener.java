20a06c8c7a3598f5ee2a365bc971989fbde03c67
JD Hernandez <jhernandez@stratpoint.com>
2016-08-08 11:39:24 +0800

    Made response receipt callbacks generic

--- app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/network/DotaApiResponseListener.java
+++ app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/network/DotaApiResponseListener.java
@@ -1,15 +1,12 @@
 package com.stratpoint.jdhrnndz.dota2junkie.network;
 
 import com.android.volley.VolleyError;
-import com.stratpoint.jdhrnndz.dota2junkie.MatchHistory;
 
 /**
  * Created by johndeniellehernandez on 8/4/16.
  */
 public interface DotaApiResponseListener {
-    void onReceiveStringResponse(int statusCode, String response);
-
-    void onReceiveMatchHistoryResponse(int statusCode, MatchHistory response);
+    void onReceiveResponse(int statusCode, Object response, int type);
 
     void onReceiveErrorResponse(int statusCode, VolleyError error);
 }
