5cb08252c1df53905ab671a5d6e11c76cea971de
JD Hernandez <jhernandez@stratpoint.com>
2016-08-09 14:24:14 +0800

    Created HeroReference model for json conversion of string saved in SharedPreferences

--- app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/model/HeroReference.java
+++ app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/model/HeroReference.java
@@ -1,7 +1,48 @@
 package com.stratpoint.jdhrnndz.dota2junkie.model;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
- * Created by johndeniellehernandez on 8/9/16.
+ * Author: John Denielle F. Hernandez
+ * Date: 8/9/16
+ * Description: The class used in GsonRequest to convert SharedPreference string into a Java Object.
  */
 public class HeroReference {
+    @SerializedName("heroes")
+    private Hero[] heroes;
+
+    public Hero[] getHeroes() {
+        return heroes;
+    }
+
+    public Hero getHero(int id) {
+        for(Hero h : heroes) {
+            if(h.getId() == id) return h;
+        }
+
+        return null;
+    }
+
+    public static class Hero {
+        @SerializedName("name")
+        private String name;
+
+        @SerializedName("id")
+        private int id;
+
+        @SerializedName("localized_name")
+        private String localizedName;
+
+        public String getName() {
+            return name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getLocalizedName() {
+            return localizedName;
+        }
+    }
 }
