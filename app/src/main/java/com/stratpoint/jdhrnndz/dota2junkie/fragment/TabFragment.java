4446f8ef0229e92a1475c0fc45ba206d5c61e0ae
JD Hernandez <jhernandez@stratpoint.com>
2016-08-11 14:11:37 +0800

    Added position value in TabFragment enum values, and getFragment and getPosition static methods

--- app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/fragment/TabFragment.java
+++ app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/fragment/TabFragment.java
@@ -8,16 +8,18 @@ import android.support.v4.app.Fragment;
  * Description: Enums used to store and provide access to fragments.
  */
 public enum TabFragment {
-    PROFILE ("Profile", ProfileFragment.newInstance()),
-    MATCHES ("Matches", MatchesFragment.newInstance()),
-    PLAY_STYLE ("Play Style", PlayStyleFragment.newInstance());
+    PROFILE ("Profile", ProfileFragment.newInstance(), 0),
+    MATCHES ("Matches", MatchesFragment.newInstance(), 1),
+    PLAY_STYLE ("Play Style", PlayStyleFragment.newInstance(), 2);
 
     private String title;
     private Fragment fragment;
+    private int position;
 
-    TabFragment(String title, Fragment fragment) {
+    TabFragment(String title, Fragment fragment, int position) {
         this.title = title;
         this.fragment = fragment;
+        this.position = position;
     }
 
     public String getTitle() {
@@ -26,5 +28,35 @@ public enum TabFragment {
 
     public Fragment getFragment() {
         return this.fragment;
+    }
+
+    public int getPosition() {
+        return this.position;
+    }
+
+    public static String getTitle(int position) {
+        String mTitle = "";
+
+        for (TabFragment tf : TabFragment.values()) {
+            if (tf.getPosition() == position) {
+                mTitle = tf.getTitle();
+                break;
+            }
+        }
+
+        return mTitle;
+    }
+
+    public static Fragment getFragment(int position) {
+        Fragment mFragment = null;
+
+        for (TabFragment tf : TabFragment.values()) {
+            if (tf.getPosition() == position) {
+                mFragment = tf.getFragment();
+                break;
+            }
+        }
+
+        return mFragment;
     }
 }
