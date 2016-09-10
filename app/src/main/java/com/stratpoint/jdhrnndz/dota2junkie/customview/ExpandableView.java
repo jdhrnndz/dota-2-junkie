026f55351d6fc134841106a81510449456bc6b96
JD Hernandez <jhernandez@stratpoint.com>
2016-09-10 16:07:11 +0800

    ExpandableView animation fix and refactoring; Code cleanup

--- app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/customview/ExpandableView.java
+++ app/src/main/java/com/stratpoint/jdhrnndz/dota2junkie/customview/ExpandableView.java
@@ -2,14 +2,12 @@ package com.stratpoint.jdhrnndz.dota2junkie.customview;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.Transformation;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.stratpoint.jdhrnndz.dota2junkie.R;
 
@@ -19,12 +17,11 @@ import com.stratpoint.jdhrnndz.dota2junkie.R;
  * Description: Custom view used to achieve an accordion effect in a layout.
  */
 public class ExpandableView extends LinearLayout implements View.OnClickListener {
-    private View mTopView, mBottomView;
+    private View mBottomView;
     private Boolean mIsExpanded;
-    private Context mContext;
-    private LayoutInflater mInflater;
     private int mBottomViewHeight;
     private boolean mIsHeightSet;
+    private AccordionAnimation mExpandAnimation, mCollapseAnimation;
 
     public ExpandableView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -34,22 +31,22 @@ public class ExpandableView extends LinearLayout implements View.OnClickListener
 
     private void init(Context context, AttributeSet attrs) {
         setOrientation(LinearLayout.VERTICAL);
+        setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
 
-        mInflater = LayoutInflater.from(context);
-        mInflater.inflate(R.layout.expandable_view, this, true);
+        LayoutInflater mInflater = LayoutInflater.from(context);
 
-        mIsExpanded = false;
-        mIsHeightSet = false;
-        mContext = context;
+        mIsExpanded = false; // state of the expandable view; initial state: collapsed
+        mIsHeightSet = false; // makes measuring bottom layout done only once
 
-        TypedArray a = mContext.getTheme().obtainStyledAttributes(
+        TypedArray a = context.getTheme().obtainStyledAttributes(
                 attrs,
                 R.styleable.ExpandableView,
                 0, 0);
 
+        View mTopView;
         try {
-            mTopView = mInflater.inflate(a.getResourceId(R.styleable.ExpandableView_topLayout, R.layout.stub_hero_details), null, false);
-            mBottomView = mInflater.inflate(a.getResourceId(R.styleable.ExpandableView_bottomLayout, R.layout.stub_hero_details), null, false);
+            mTopView = mInflater.inflate(a.getResourceId(R.styleable.ExpandableView_topLayout, 0), null, false);
+            mBottomView = mInflater.inflate(a.getResourceId(R.styleable.ExpandableView_bottomLayout, 0), null, false);
         } finally {
             a.recycle();
         }
@@ -59,21 +56,25 @@ public class ExpandableView extends LinearLayout implements View.OnClickListener
 
         mBottomView.setVisibility(GONE);
         setOnClickListener(this /** OnClickListener **/);
-    }
-
-    private void toggleView() {
-        mIsExpanded = !mIsExpanded;
+        mExpandAnimation = new ExpandAnimation(mBottomView);
+        mCollapseAnimation = new CollapseAnimation(mBottomView);
     }
 
     @Override
     public void onClick(View view) {
         if (!mIsHeightSet) {
             mBottomViewHeight = measureBottomView();
+            int animDuration = ((int)(mBottomViewHeight / mBottomView.getContext().getResources().getDisplayMetrics().density)) * 4;
+            mExpandAnimation.setDuration(animDuration);
+            mCollapseAnimation.setDuration(animDuration);
             mIsHeightSet = true;
         }
-        animate(mBottomView, !mIsExpanded);
 
         this.toggleView();
+    }
+
+    private void toggleView() {
+        animate(mIsExpanded = !mIsExpanded);
     }
 
     private int measureBottomView() {
@@ -92,41 +93,66 @@ public class ExpandableView extends LinearLayout implements View.OnClickListener
         return bottomViewHeight;
     }
 
-    private void animate(final View view, final boolean isExpand) {
-        if (isExpand) {
-            view.setVisibility(VISIBLE);
+    private void animate(boolean isExpanded) {
+        if (isExpanded) {
+            mBottomView.getLayoutParams().height = 1;
+            mBottomView.setVisibility(VISIBLE);
+            mBottomView.startAnimation(mExpandAnimation);
+        } else {
+            mBottomView.startAnimation(mCollapseAnimation);
+        }
+    }
+
+    class ExpandAnimation extends AccordionAnimation {
+        public ExpandAnimation(View view) {
+            super(view);
         }
 
-        Animation a = new Animation() {
-            @Override
-            protected void applyTransformation(float interpolatedTime, Transformation t) {
-                if (Float.compare(interpolatedTime, 1.0f) == 0) {
-                    if (isExpand) {
-                        view.getLayoutParams().height = LayoutParams.WRAP_CONTENT;
-                    } else {
-                        view.setVisibility(GONE);
-                    }
-                } else {
-                    view.getLayoutParams().height = (isExpand) ?
-                            (int) (mBottomViewHeight * interpolatedTime) :
-                            (int) (mBottomViewHeight - (mBottomViewHeight * interpolatedTime));
-                    view.requestLayout();
-                }
+        @Override
+        protected void applyTransformation(float interpolatedTime, Transformation t) {
+            if (Float.compare(interpolatedTime, 1.0f) == 0) {
+                mView.getLayoutParams().height = LayoutParams.WRAP_CONTENT;
+            } else {
+                mView.getLayoutParams().height = (int) (mBottomViewHeight * interpolatedTime);
+                mView.requestLayout();
             }
+        }
+    }
 
-            @Override
-            public void initialize(int width, int height, int parentWidth,
-                                   int parentHeight) {
-                super.initialize(width, height, parentWidth, parentHeight);
+    class CollapseAnimation extends AccordionAnimation {
+        public CollapseAnimation(View view) {
+            super(view);
+        }
+
+        @Override
+        protected void applyTransformation(float interpolatedTime, Transformation t) {
+            if (Float.compare(interpolatedTime, 1.0f) == 0) {
+                mView.setVisibility(GONE);
+            } else {
+                mView.getLayoutParams().height = (int) (mBottomViewHeight - (mBottomViewHeight * interpolatedTime));
+                mView.requestLayout();
             }
+        }
+    }
 
-            @Override
-            public boolean willChangeBounds() {
-                return true;
-            }
-        };
+    class AccordionAnimation extends Animation {
+        protected View mView;
 
-        a.setDuration(((int)(mBottomViewHeight / view.getContext().getResources().getDisplayMetrics().density)) * 4 );
-        view.startAnimation(a);
+        // Constructor to set view to animate/get measurements from
+        public AccordionAnimation(View view) {
+            super();
+
+            mView = view;
+        }
+
+        @Override
+        public void initialize(int width, int height, int parentWidth, int parentHeight) {
+            super.initialize(width, height, parentWidth, parentHeight);
+        }
+
+        @Override
+        public boolean willChangeBounds() {
+            return true;
+        }
     }
 }
